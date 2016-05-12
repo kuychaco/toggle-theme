b83f23a79b8618b27cea29a535f448b7186cced9
Katrina Uychaco <kuychaco@github.com>
2016-05-12 19:28:52 +0200

    initial commit

---
+++ spec/toggle-theme-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import ToggleThemeView from '../lib/toggle-theme-view';
+
+describe('ToggleThemeView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
